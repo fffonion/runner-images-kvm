ae8ab25f0c4a48e5735a37707f5237f16c730398
fffonion <fffonion@gmail.com>
2023-08-17 18:36:43 +0200

    [kvm] multi arch installer

--- images/ubuntu/scripts/build/install-github-cli.sh
+++ images/ubuntu/scripts/build/install-github-cli.sh
@@ -3,20 +3,14 @@
 ##  File:  install-github-cli.sh
 ##  Desc:  Install GitHub CLI
 ##         Must be run as non-root user after homebrew
-##  Supply chain security: GitHub CLI - checksum validation
 ################################################################################
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
-# Download GitHub CLI
-URL=$(get_github_package_download_url "cli/cli" "contains(\"linux\") and contains(\"amd64\") and contains(\".deb\")")
-download_with_retries "${URL}" "/tmp" "gh_cli_linux_amd64.deb"
-# Supply chain security - GitHub CLI
-hash_url=$(get_github_package_download_url "cli/cli" "contains(\"checksums.txt\")")
-external_hash=$(get_hash_from_remote_file "${hash_url}" "linux_amd64.deb")
-use_checksum_comparison "/tmp/gh_cli_linux_amd64.deb" "${external_hash}"
 # Install GitHub CLI
-apt install /tmp/gh_cli_linux_amd64.deb
+downloadUrl=$(get_github_package_download_url "cli/cli" "contains(\"linux\") and contains(\"$ARCH\") and contains(\".deb\")")
+download_with_retries $downloadUrl "/tmp"
+apt install /tmp/gh_*_linux_*.deb
 
 invoke_tests "CLI.Tools" "GitHub CLI"
--- images/ubuntu/scripts/build/install-ruby.sh
+++ images/ubuntu/scripts/build/install-ruby.sh
@@ -47,7 +47,13 @@ for TOOLSET_VERSION in ${TOOLSET_VERSIONS[@]}; do
     echo "Expand '$PACKAGE_TAR_NAME' to the '$RUBY_VERSION_PATH' folder"
     tar xf "/tmp/$PACKAGE_TAR_NAME" -C $RUBY_VERSION_PATH
 
-    COMPLETE_FILE_PATH="$RUBY_VERSION_PATH/x64.complete"
+    if [[ $ARCH == "arm64" ]]; then
+	    arch="arm64"
+    else
+	    arch="x64"
+    fi
+
+    COMPLETE_FILE_PATH="$RUBY_VERSION_PATH/$arch.complete"
     if [ ! -f $COMPLETE_FILE_PATH ]; then
         echo "Create complete file"
         touch $COMPLETE_FILE_PATH
