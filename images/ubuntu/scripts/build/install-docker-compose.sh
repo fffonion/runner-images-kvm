2fd9e682bdb9aaab6876aab54b37837d81901802
fffonion <fffonion@gmail.com>
2023-08-17 18:36:43 +0200

    [kvm] multi arch installer

--- images/ubuntu/scripts/build/install-docker-compose.sh
+++ images/ubuntu/scripts/build/install-docker-compose.sh
@@ -8,14 +8,18 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/install.sh
 
-# Download docker-compose v1 from releases
-binary_path=$(download_with_retry "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64")
 
-# Supply chain security - Docker Compose v1
-external_hash="f3f10cf3dbb8107e9ba2ea5f23c1d2159ff7321d16f0a23051d68d8e2547b323"
-use_checksum_comparison "${binary_path}" "${external_hash}"
+if [[ $(arch) == "aarch64" ]]; then
+    sudo apt-get install -y docker-compose --no-install-recommends
+else
+    # Download docker-compose v1 from releases
+    binary_path=$(download_with_retry "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-$ARCH_L")
+    # Supply chain security - Docker Compose v1
+    external_hash="f3f10cf3dbb8107e9ba2ea5f23c1d2159ff7321d16f0a23051d68d8e2547b323"
+    use_checksum_comparison "${binary_path}" "${external_hash}"
 
-# Install docker-compose v1
-install "${binary_path}" "/usr/local/bin/docker-compose"
+    # Install docker-compose v1
+    install "${binary_path}" "/usr/local/bin/docker-compose"
 
-invoke_tests "Tools" "Docker-compose v1"
+    invoke_tests "Tools" "Docker-compose v1"
+fi
