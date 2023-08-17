a67d89cd3c5a41ba45843c85447d4185cdd919cb
fffonion <fffonion@gmail.com>
2023-08-17 18:36:43 +0200

    [kvm] multi arch installer

--- images/linux/scripts/base/apt-ubuntu-archive.sh
+++ images/linux/scripts/base/apt-ubuntu-archive.sh
@@ -4,9 +4,16 @@
 ##  Desc:  Script for adding additional apt repo to /etc/apt/sources.list and /etc/cloud/templates/sources.list.ubuntu.tmpl
 ################################################################################
 
-echo "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -cs) main restricted" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
-echo "deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -cs)-updates main restricted" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs) main restricted" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs) universe" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-updates main restricted" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-updates universe" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
 
-echo "deb http://security.ubuntu.com/ubuntu/ $(lsb_release -cs)-security main restricted" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
-echo "deb http://security.ubuntu.com/ubuntu/ $(lsb_release -cs)-security universe" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
-echo "deb http://security.ubuntu.com/ubuntu/ $(lsb_release -cs)-security multiverse" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs) multiverse" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-updates multiverse" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-backports main restricted universe multiverse" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-security main restricted" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-security universe" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
+echo "deb http://ports.ubuntu.com/ubuntu-ports/ $(lsb_release -cs)-security multiverse" | tee -a /etc/apt/sources.list /etc/cloud/templates/sources.list.ubuntu.tmpl
