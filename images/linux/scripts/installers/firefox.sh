19ec27819edddc38ea01dee0bd57fa214a92f4b7
fffonion <fffonion@gmail.com>
2023-08-17 18:36:43 +0200

    [kvm] multi arch installer

--- images/linux/scripts/installers/firefox.sh
+++ images/linux/scripts/installers/firefox.sh
@@ -25,7 +25,11 @@ echo "mozillateam $FIREFOX_REPO" >> $HELPER_SCRIPTS/apt-sources.txt
 echo 'pref("intl.locale.requested","en_US");' >> "/usr/lib/firefox/browser/defaults/preferences/syspref.js"
 
 # Download and unpack latest release of geckodriver
-downloadUrl=$(get_github_package_download_url "mozilla/geckodriver" "test(\"linux64.tar.gz$\")")
+if [[ $ARCH == "arm64" ]]; then
+	downloadUrl=$(get_github_package_download_url "mozilla/geckodriver" "test(\"linux-aarch64.tar.gz$\")")
+else
+	downloadUrl=$(get_github_package_download_url "mozilla/geckodriver" "test(\"linux64.tar.gz$\")")
+fi
 echo "Downloading geckodriver $downloadUrl"
 download_with_retries "$downloadUrl" "/tmp" geckodriver.tar.gz
 
