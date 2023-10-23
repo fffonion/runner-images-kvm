3f5ec9f0d6b8b0ea9d705fb993b921f119167207
Wangchong Zhou <fffonion@gmail.com>
2023-10-23 17:29:43 +0800

    Fix release script to include multiple repos

--- make-new-release.sh
+++ make-new-release.sh
@@ -5,19 +5,6 @@ if [[ "$OSTYPE" == darwin* ]]; then
     GREP=ggrep
 fi
 
-git reset --hard
-git checkout main
-git remote add upstream https://github.com/actions/runner-images 2>/dev/null
-git fetch upstream --tags 2>/dev/null
-git fetch origin 2>/dev/null
-rel=$(gh release list -R actions/runner-images|$GREP -oP "ubuntu22/[\d\.]+"|head -n1)
-last_kvm_branch=origin/$(git branch|grep kvm|grep -v arm64|grep -v $rel|sort|tail -n1|awk '{print $1}')
-last_arm64_branch=origin/$(git branch|grep kvm-arm64|grep -v $rel|sort|tail -n1|awk '{print $1}')
-
-echo "The latest upstream release tag is $rel"
-
-git checkout $rel
-
 cherry() {
     c=$1
     git cherry-pick $c
@@ -27,13 +14,33 @@ cherry() {
     done
 }
 
+push() {
+    git push origin $@
+    git push private $@
+}
+
+
+git reset --hard
+git checkout main
+git remote add upstream https://github.com/actions/runner-images 2>/dev/null
+git fetch upstream --tags 2>/dev/null
+git fetch origin 2>/dev/null
+
+rel=$(gh release list -R actions/runner-images|$GREP -oP "ubuntu22/[\d\.]+"|head -n1)
+last_kvm_branch=origin/$(git branch|grep kvm|grep -v arm64|grep -v $rel|sort|tail -n1|awk '{print $1}')
+last_arm64_branch=origin/$(git branch|grep kvm-arm64|grep -v $rel|sort|tail -n1|awk '{print $1}')
+
+echo "The latest upstream release tag is $rel"
+
+git checkout $rel
+
 git branch -D ${rel}-kvm 2>/dev/null
 git checkout -b ${rel}-kvm 2>/dev/null
 git clean -f
 cherry $(git log -n 1 --pretty=format:"%H" $last_kvm_branch)
-git push origin -f refs/heads/${rel}-kvm:refs/heads/${rel}-kvm
+push -f refs/heads/${rel}-kvm:refs/heads/${rel}-kvm
 git tag -f ${rel}
-git push origin -f refs/tags/${rel}:refs/tags/${rel}
+push -f refs/tags/${rel}:refs/tags/${rel}
 gh release create $rel --notes "https://github.com/actions/runner-images/releases/tag/${rel//\//%2F}"
 
 git branch -D ${rel}-kvm-arm64 2>/dev/null
@@ -42,8 +49,8 @@ git clean -f
 for c in $(git log --reverse -n 2 --pretty=format:"%H" $last_arm64_branch); do
     cherry $c
 done
-git push origin -f refs/heads/${rel}-kvm-arm64:refs/heads/${rel}-kvm-arm64
+push origin -f refs/heads/${rel}-kvm-arm64:refs/heads/${rel}-kvm-arm64
 git tag -f ${rel}-arm64
-git push origin -f refs/tags/${rel}-arm64:refs/tags/${rel}-arm64
+push origin -f refs/tags/${rel}-arm64:refs/tags/${rel}-arm64
 gh release create $rel-arm64 --notes "https://github.com/actions/runner-images/releases/tag/${rel//\//%2F}"
 
