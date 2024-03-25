a3ad4248c3fa3910e8aeef432b15311892837118
Wangchong Zhou <wangchong@konghq.com>
2024-03-25 15:22:32 +0800

    [scripts] always pull latest origin

--- make-new-release.sh
+++ make-new-release.sh
@@ -37,6 +37,7 @@ git checkout $rel
 git branch -D ${rel}-kvm 2>/dev/null
 git checkout -b ${rel}-kvm 2>/dev/null
 git clean -f
+git pull origin ${rel}-kvm --rebase 2>/dev/null
 for c in $(git log --reverse -n 2 --pretty=format:"%H" $last_kvm_branch); do
     cherry $c
 done
@@ -48,6 +49,7 @@ gh release create $rel --notes "https://github.com/actions/runner-images/release
 git branch -D ${rel}-kvm-arm64 2>/dev/null
 git checkout -b ${rel}-kvm-arm64 2>/dev/null
 git clean -f
+git pull origin ${rel}-kvm-arm64 --rebase 2>/dev/null
 # total of 4 commits for arm64
 for c in $(git log --reverse -n 2 --pretty=format:"%H" $last_arm64_branch); do
     cherry $c
